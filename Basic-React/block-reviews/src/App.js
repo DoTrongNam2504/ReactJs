8930cef579591bdbe091b47ae31ed3baee56bd72
namdt <dotrongnam2307200@gmail.com>
2023-02-14 16:16:18 +0700

    add source code project blockreview

---
+++ Basic-React/block-reviews/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Reviews from './Reviews';
+import './App.css';
+
+function App() {
+
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline" />
+        </div>
+        <Reviews />
+      </section>
+    </main>
+
+  );
+}
+
+export default App;
